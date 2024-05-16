65949f74aa587788d55f5928d8d93a15c731a97a
hicham-oualla <hich2022am.oualla@gmail.com>
2024-05-16 17:02:36 +0100

    configuration du hybernate

---
+++ BudgetManager/src/main/java/com/model/utulisateur.java
@@ -0,0 +1,33 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class utulisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    private String name;
+
+    // Getters and setters
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
